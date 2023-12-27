0f4a8a3ddf4dc6d7dc8b96dc4ac25d3ec6882b22
Md Asif Khan <asifkhan88012@gmail.com>
2023-12-27 12:42:09 +0530

    created about js component and added initial level dark mode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import Textbox from './Components/Textbox';
+import About from './Components/About';
 
 function App() {
   return (
    <>
     <Navbar title = "TestUtils"/>
-    <div className="container">
+    {/* <div className="container">
       <Textbox heading = "Enter Your Text Here..."/>
-    </div>
+    </div> */}
+   <About />
+
    </>
+
 
   );
 }
