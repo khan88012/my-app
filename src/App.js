9080eeba6f7ff915c31020a9a19757ec79bf9ee2
Md Asif Khan <asifkhan88012@gmail.com>
2023-12-27 15:21:29 +0530

    added theming in the textbox window

--- src/App.js
+++ src/App.js
@@ -2,15 +2,32 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Textbox from './Components/Textbox';
 import About from './Components/About';
+import { useState } from 'react';
 
 function App() {
+  const [mode , setMode]= useState('light');
+
+  const toggleMode = () =>{
+    console.log('toggle theme clicked')
+    if(mode === 'light')
+    {
+      setMode('dark');
+      document.body.style.backgroundColor = 'grey';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+
+
   return (
    <>
-    <Navbar title = "TestUtils"/>
-    {/* <div className="container">
-      <Textbox heading = "Enter Your Text Here..."/>
-    </div> */}
-   <About />
+    <Navbar title = "TextUtils" mode={mode} toggleMode={toggleMode}/>
+    <div className="container">
+      <Textbox heading = "Enter Your Text Here..." mode={mode}/>
+    </div>
+   {/* <About /> */}
 
    </>
 
--- src/Components/Textbox.js
+++ src/Components/Textbox.js
@@ -1,40 +1,50 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 export default function Textbox(props) {
-    const[text , setText] = useState('Enter Text Here');
-    const handleUpClick = () =>{
-        let newText = text.toUpperCase();
-        setText(newText);
-    }
-    const handlLoClick = () =>{
-        let newText = text.toLowerCase();
-        setText(newText);
-    }
+  const [text, setText] = useState("");
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handlLoClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
 
-    const handleOnChange = (event) =>{
-        setText(event.target.value);
-    }
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
 
   return (
     <>
-    <h1 className="my-3">{props.heading}</h1>
-      <div className="form-floating">
-        <textarea
-          className="form-control"
-          placeholder="Leave a comment here"
-          value={text}
-          id="TextBox"
-          onChange={handleOnChange}
-        ></textarea>
-      </div>
-      <button className="btn btn-primary my-3" onClick={handleUpClick}> Convert To Uppercase</button>
-      <button className="btn btn-primary mx-3" onClick={handlLoClick}> Convert To Lowecase</button>
+      <div style={{color: props.mode === 'light'?'black':'white'}}>
+        <h1 className="my-3">{props.heading}</h1>
+        <div className="form-floating">
+          <textarea
+            className="form-control"
+            placeholder="Leave a comment here"
+            value={text}
+            id="TextBox"
+            onChange={handleOnChange}
+          ></textarea>
+        </div>
+        <button className="btn btn-primary my-3" onClick={handleUpClick}>
+          {" "}
+          Convert To UpperCase
+        </button>
+        <button className="btn btn-primary mx-3" onClick={handlLoClick}>
+          {" "}
+          Convert To Lowercase
+        </button>
 
-      <h1>Text Summary</h1>
-      <p>{text.split(' ').length} words and {text.length} letters.</p>
-      <p>{0.008 * text.split(' ').length} minutes to read</p>
-      <h2>Preview</h2>
-      <p>{text}</p>
+        <h1>Text Summary</h1>
+        <p>
+          {text.split(" ").length} words and {text.length} letters.
+        </p>
+        <p>{0.008 * text.split(" ").length} minutes to read</p>
+        <h2>Preview</h2>
+        <p>{text.length > 0?text:'Enter Text Above To Check The Preview'}</p>
+      </div>
     </>
   );
 }
